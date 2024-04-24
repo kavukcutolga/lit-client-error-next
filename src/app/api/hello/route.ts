dc3f3b4f52a7e454ab17effc76b570cb585c6eba
tolgakavukcu <kavukcu.tolga@gmail.com>
2024-04-24 20:58:49 +0200

    Initial commit

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,5 +1,13 @@
+import * as LitJsSdk from "@lit-protocol/lit-node-client";
 import { NextResponse } from 'next/server';
 
+const litNodeClient = new LitJsSdk.LitNodeClientNodeJs({
+  alertWhenUnauthorized: false,
+  litNetwork: 'habanero',
+});
+
+
 export async function GET() {
+  await litNodeClient.connect();
   return NextResponse.json({ hello: 'Next.js' });
 }
